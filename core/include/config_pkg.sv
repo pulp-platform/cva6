84c5185476fab702aff75ce77f3b304307c34b4e
Nils Wistoff <nwistoff@iis.ee.ethz.ch>
2023-04-05 19:46:34 +0200

    Add CLIC support (#10)
    * Add initial CLIC support
    
    Co-authored-by: aottaviano <aottaviano@iis.ee.ethz.ch>
    
    * core: Fix propagation of CLIC configuration
    
    * clic: Fix syntax, preserve old irq signals
    
    * clic/ariane_testharness: Fix CVA6 portlist
    
    * fixup! clic/ariane_testharness: Fix CVA6 portlist
    
    * fixup! fixup! clic/ariane_testharness: Fix CVA6 portlist
    
    * clic/ariane_testharness: Define `meip` and `seip`
    
    * csr_regfile: Update `stvec` for CLIC
    
    * csr_regfile: Write CLIC mode to `mtvec` only if en
    
    Signed-off-by: Nils Wistoff <nwistoff@iis.ee.ethz.ch>
    
    * ci: Disable CLIC test
    
    * csr_regfile: Add `sintstatus` and `sintthresh` CSR
    
    * clic/id_stage: Add lower priv irq accept logic
    
    * fixup! clic/id_stage: Add lower priv irq accept logic
    
    * ariane_testharness: Unify CLIC and CLINT versions
    
    * Makefile: Add missing rv_plic dependency for clic
    
    * ariane_soc_pkg: Enable CLIC
    
    * Revert "ci: Disable CLIC test"
    
    This reverts commit 4195d3f56b0eb60359b6120b835c21cd0afed4a5.
    
    * sram: Tie unused user signal to `0`
    
    to avoid undriven user signals in the AXI interconnect
    
    * id_stage: Ignore mie CSR when taking interrupts
    
    mstatus.mie is checked via `global_enable` within the decoder
    
    * ariane_tetsharness: Connect mtip and msip to CLIC
    
    * cva6_config_pkg: Disable CVXIF
    
    * ci: Use clic branch of riscv-tests
    
    * clic: Change one-hot to ID irq
    
    * clic: Aggregate core's CLIC logic in new module
    
    * clic: Add kill handshake
    
    * fixup! clic: Add kill handshake
    
    * decoder: Remove unused block
    
    * clic: Guard additions with param
    
    * ariane_xilinx: Unify CLIC and CLINT modes
    
    * clic: Remove mclicbase
    
    According to the updated spec
    
    * csr_regfile: Read 0, ignore writes from CLINT mode
    
    * clic: Stilistic fixes
    
    * csr_regfile: Add mtvt disclaimer
    
    * ci: Point to pulp branch
    
    * Revert "sram: Tie unused user signal to `0`"
    
    This reverts commit add8320ee241b2c21713474cd04c98e445aadb36.
    Will be merged separately
    
    * ariane_pkg: Change struct param to pkg param
    
    * clic: Bump
    
    * Makefile: Update for clic
    
    * ariane_testharness: Connect handshake kill logic
    
    * ariane_xilinx: Connect handshake kill logic
    
    * ariane_soc_pkg: Reduce clic interrupts to 64
    
    * clic: Bump
    
    * ariane_testharness: Fix zero fill size for irqs
    
    * ariane_testharness/xilinx: Add unused cva6 ports
    
    * clic: Update CI
    
    * clic: Save/restore sil to/from scause
    
    * ci: Align clic testcase naming
    
    ---------
    
    Signed-off-by: Nils Wistoff <nwistoff@iis.ee.ethz.ch>
    Co-authored-by: aottaviano <aottaviano@iis.ee.ethz.ch>
    Co-authored-by: bluew <bluewww@users.noreply.github.com>
    Co-authored-by: Enrico Zelioli <enricozelioli97@gmail.com>
    Signed-off-by: Nils Wistoff <nwistoff@iis.ee.ethz.ch>

--- core/include/config_pkg.sv
+++ core/include/config_pkg.sv
@@ -62,6 +62,8 @@ package config_pkg;
     bit                          RVZCB;
     // Zcmp RISC-V extension
     bit                          RVZCMP;
+    // CLIC extension
+    bit                          RVSCLIC;
     // Zicond RISC-V extension
     bit                          RVZiCond;
     // Zicntr RISC-V extension
@@ -130,6 +132,8 @@ package config_pkg;
     bit                          CvxifEn;
     // NOC bus type
     noc_type_e                   NOCType;
+    // Number of interrupt signals from the CLIC.
+    int unsigned                 CLICNumInterruptSrc;
     // AXI address width
     int unsigned                 AxiAddrWidth;
     // AXI data width
