59bcfcc13d6ebd9ef818b168c857ce4e309d3a9b
Nils Wistoff <nwistoff@iis.ee.ethz.ch>
2023-04-05 19:46:34 +0200

    Add CLIC support (#10)
    * Add initial CLIC support
    
    Co-authored-by: aottaviano <aottaviano@iis.ee.ethz.ch>
    
    * core: Fix propagation of CLIC configuration
    
    * clic: Fix syntax, preserve old irq signals
    
    * clic/ariane_testharness: Fix CVA6 portlist
    
    * fixup! clic/ariane_testharness: Fix CVA6 portlist
    
    * fixup! fixup! clic/ariane_testharness: Fix CVA6 portlist
    
    * clic/ariane_testharness: Define `meip` and `seip`
    
    * csr_regfile: Update `stvec` for CLIC
    
    * csr_regfile: Write CLIC mode to `mtvec` only if en
    
    Signed-off-by: Nils Wistoff <nwistoff@iis.ee.ethz.ch>
    
    * ci: Disable CLIC test
    
    * csr_regfile: Add `sintstatus` and `sintthresh` CSR
    
    * clic/id_stage: Add lower priv irq accept logic
    
    * fixup! clic/id_stage: Add lower priv irq accept logic
    
    * ariane_testharness: Unify CLIC and CLINT versions
    
    * Makefile: Add missing rv_plic dependency for clic
    
    * ariane_soc_pkg: Enable CLIC
    
    * Revert "ci: Disable CLIC test"
    
    This reverts commit 4195d3f56b0eb60359b6120b835c21cd0afed4a5.
    
    * sram: Tie unused user signal to `0`
    
    to avoid undriven user signals in the AXI interconnect
    
    * id_stage: Ignore mie CSR when taking interrupts
    
    mstatus.mie is checked via `global_enable` within the decoder
    
    * ariane_tetsharness: Connect mtip and msip to CLIC
    
    * cva6_config_pkg: Disable CVXIF
    
    * ci: Use clic branch of riscv-tests
    
    * clic: Change one-hot to ID irq
    
    * clic: Aggregate core's CLIC logic in new module
    
    * clic: Add kill handshake
    
    * fixup! clic: Add kill handshake
    
    * decoder: Remove unused block
    
    * clic: Guard additions with param
    
    * ariane_xilinx: Unify CLIC and CLINT modes
    
    * clic: Remove mclicbase
    
    According to the updated spec
    
    * csr_regfile: Read 0, ignore writes from CLINT mode
    
    * clic: Stilistic fixes
    
    * csr_regfile: Add mtvt disclaimer
    
    * ci: Point to pulp branch
    
    * Revert "sram: Tie unused user signal to `0`"
    
    This reverts commit add8320ee241b2c21713474cd04c98e445aadb36.
    Will be merged separately
    
    * ariane_pkg: Change struct param to pkg param
    
    * clic: Bump
    
    * Makefile: Update for clic
    
    * ariane_testharness: Connect handshake kill logic
    
    * ariane_xilinx: Connect handshake kill logic
    
    * ariane_soc_pkg: Reduce clic interrupts to 64
    
    * clic: Bump
    
    * ariane_testharness: Fix zero fill size for irqs
    
    * ariane_testharness/xilinx: Add unused cva6 ports
    
    * clic: Update CI
    
    * clic: Save/restore sil to/from scause
    
    * ci: Align clic testcase naming
    
    ---------
    
    Signed-off-by: Nils Wistoff <nwistoff@iis.ee.ethz.ch>
    Co-authored-by: aottaviano <aottaviano@iis.ee.ethz.ch>
    Co-authored-by: bluew <bluewww@users.noreply.github.com>
    Co-authored-by: Enrico Zelioli <enricozelioli97@gmail.com>
    Signed-off-by: Nils Wistoff <nwistoff@iis.ee.ethz.ch>

--- core/include/cv64a6_imafdchsclic_sv39_config_pkg.sv
+++ core/include/cv64a6_imafdchsclic_sv39_config_pkg.sv
@@ -6,13 +6,6 @@
 // You may obtain a copy of the License at https://solderpad.org/licenses/
 //
 // Original Author: Jean-Roch COULON - Thales
-//
-// Copyright 2023 Commissariat a l'Energie Atomique et aux Energies
-//                Alternatives (CEA)
-//
-// Author: Cesar Fuguet - CEA
-// Date: August, 2023
-// Description: CVA6 configuration package using the HPDcache as cache subsystem
 
 
 package cva6_config_pkg;
@@ -23,18 +16,19 @@ package cva6_config_pkg;
   localparam CVA6ConfigF16En = 0;
   localparam CVA6ConfigF16AltEn = 0;
   localparam CVA6ConfigF8En = 0;
+  localparam CVA6ConfigF8AltEn = 0;
   localparam CVA6ConfigFVecEn = 0;
 
-  localparam CVA6ConfigCvxifEn = 1;
+  localparam CVA6ConfigCvxifEn = 0;
   localparam CVA6ConfigCExtEn = 1;
   localparam CVA6ConfigZcbExtEn = 1;
   localparam CVA6ConfigZcmpExtEn = 0;
   localparam CVA6ConfigAExtEn = 1;
-  localparam CVA6ConfigBExtEn = 1;
+  localparam CVA6ConfigHExtEn = 1;
+  localparam CVA6ConfigBExtEn = 0;
   localparam CVA6ConfigVExtEn = 0;
-  localparam CVA6ConfigHExtEn = 0;
   localparam CVA6ConfigRVZiCond = 1;
-  localparam CVA6ConfigSclicExtEn = 0;
+  localparam CVA6ConfigSclicExtEn = 1;
 
   localparam CVA6ConfigAxiIdWidth = 4;
   localparam CVA6ConfigAxiAddrWidth = 64;
@@ -42,7 +36,7 @@ package cva6_config_pkg;
   localparam CVA6ConfigFetchUserEn = 0;
   localparam CVA6ConfigFetchUserWidth = CVA6ConfigXlen;
   localparam CVA6ConfigDataUserEn = 0;
-  localparam CVA6ConfigDataUserWidth = CVA6ConfigXlen;
+  localparam CVA6ConfigDataUserWidth = 1;
 
   localparam CVA6ConfigIcacheByteSize = 16384;
   localparam CVA6ConfigIcacheSetAssoc = 4;
@@ -51,8 +45,8 @@ package cva6_config_pkg;
   localparam CVA6ConfigDcacheSetAssoc = 8;
   localparam CVA6ConfigDcacheLineWidth = 128;
 
-  localparam CVA6ConfigDcacheIdWidth = 3;
-  localparam CVA6ConfigMemTidWidth = CVA6ConfigAxiIdWidth;
+  localparam CVA6ConfigDcacheIdWidth = 1;
+  localparam CVA6ConfigMemTidWidth = 2;
 
   localparam CVA6ConfigWtDcacheWbufDepth = 8;
 
@@ -62,7 +56,7 @@ package cva6_config_pkg;
 
   localparam CVA6ConfigNrLoadPipeRegs = 1;
   localparam CVA6ConfigNrStorePipeRegs = 0;
-  localparam CVA6ConfigNrLoadBufEntries = 8;
+  localparam CVA6ConfigNrLoadBufEntries = 2;
 
   localparam CVA6ConfigRASDepth = 2;
   localparam CVA6ConfigBTBEntries = 32;
@@ -74,7 +68,7 @@ package cva6_config_pkg;
 
   localparam CVA6ConfigPerfCounterEn = 1;
 
-  localparam config_pkg::cache_type_t CVA6ConfigDcacheType = config_pkg::HPDCACHE;
+  localparam config_pkg::cache_type_t CVA6ConfigDcacheType = config_pkg::WT;
 
   localparam CVA6ConfigMmuPresent = 1;
 
@@ -97,6 +91,7 @@ package cva6_config_pkg;
       XF16: bit'(CVA6ConfigF16En),
       XF16ALT: bit'(CVA6ConfigF16AltEn),
       XF8: bit'(CVA6ConfigF8En),
+      XF8ALT: bit'(CVA6ConfigF8AltEn),
       RVA: bit'(CVA6ConfigAExtEn),
       RVB: bit'(CVA6ConfigBExtEn),
       RVV: bit'(CVA6ConfigVExtEn),
