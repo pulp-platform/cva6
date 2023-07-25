75986cc0317918b811d8425673cff9824ba93a35
Nils Wistoff <nwistoff@iis.ee.ethz.ch>
2023-07-25 20:52:55 +0200

    Add self-invalidation coherence
    added files required for compilation
    
    Added target to test litmus tests
    
    Per byte dirty bit added to std dcache and tested
    
    basic support for dual core instantiation
    
    Some automation added to the multi-core testing process
    
    minor changes
    
    temporary ci modifications for working without sudo permissions
    
    branch prova
    
    prova modified
    
    Multi core instantiation made generic
    
    Transition between WAIT_CRITICAL_WORD and WAIT_TAG removed if there is a flush - feature tested
    
    Changed repo with master branch and added masks for reservation at cacheline granularity because burst not supported
    
    Fix the never return problem for non boot cores and dt modified for 2 cores
    
    Added master branch of common_cells and compilation of new file in Makefile
    
    Added transition between FLUSHING and FLUSHING to avoid multiple flushs during atomics
    
    Unused code removed and code commented
    
    Added support to use the master branch of the axi_riscv_atomics repository
    
    Added support for multiple ariane instances for fpga synthesis
    
    Increased stack for big applications and reduced number of harts
    
    Pheripherals configured to use multiple cores
    
    List of issues not solved encountered during the master thesis
    
    Co-authored-by: msc22h2 <s290705@studenti.polito.it>
    Signed-off-by: Nils Wistoff <nwistoff@iis.ee.ethz.ch>

--- core/cache_subsystem/cache_ctrl.sv
+++ core/cache_subsystem/cache_ctrl.sv
@@ -319,11 +319,11 @@ module cache_ctrl
 
           // set the correct byte enable
           be_o.data[cl_offset>>3+:CVA6Cfg.XLEN/8] = mem_req_q.be;
-          data_o.data[cl_offset+:CVA6Cfg.XLEN]    = mem_req_q.wdata;
-          data_o.tag                              = mem_req_d.tag;
+          data_o.data[cl_offset+:CVA6Cfg.XLEN]       = mem_req_q.wdata;
+          data_o.tag                                 = mem_req_d.tag;
           // ~> change the state
-          data_o.dirty                            = 1'b1;
-          data_o.valid                            = 1'b1;
+          data_o.dirty[cl_offset>>3+:CVA6Cfg.XLEN/8] = 1'b1;
+          data_o.valid                               = 1'b1;
 
           // got a grant ~> this is finished now
           if (gnt_i) begin
