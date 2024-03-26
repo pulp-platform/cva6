54c2395fe3f9dbb176111a2e754c782c936572b2
Enrico Zelioli <ezelioli@iis.ee.ethz.ch>
2024-03-26 16:56:56 +0100

    fix rebase:
    revert change to AXI ID signal in request interface towards data cache

--- core/cache_subsystem/cache_ctrl.sv
+++ core/cache_subsystem/cache_ctrl.sv
@@ -132,7 +132,7 @@ module cache_ctrl
     req_port_o.data_gnt = 1'b0;
     req_port_o.data_rvalid = 1'b0;
     req_port_o.data_rdata = '0;
-    req_port_o.data_rid = '0; // req_port_o.data_rid = mem_req_q.id;
+    req_port_o.data_rid = mem_req_q.id;
     miss_req_o = '0;
     mshr_addr_o = '0;
     // Memory array communication
