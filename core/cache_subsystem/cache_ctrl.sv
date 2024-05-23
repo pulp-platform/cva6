d2ecbdef58fc4141f0ee55ca7b051eaaaac3ba34
Enrico Zelioli <ezelioli@iis.ee.ethz.ch>
2024-05-23 17:52:47 +0200

    Fix bug in WB cache controller
    Wait for valid signal from miss handler when killing bypass memory
    transaction before going to IDLE.

--- core/cache_subsystem/cache_ctrl.sv
+++ core/cache_subsystem/cache_ctrl.sv
@@ -538,7 +538,7 @@ module cache_ctrl
 
     if (req_port_i.kill_req) begin
       req_port_o.data_rvalid = 1'b1;
-      if (!(state_q inside {MAKE_UNIQUE, WAIT_REFILL_GNT, WAIT_CRITICAL_WORD})) begin
+      if (!(state_q inside {MAKE_UNIQUE, WAIT_REFILL_GNT, WAIT_REFILL_VALID, WAIT_CRITICAL_WORD})) begin
         state_d = IDLE;
       end
     end
