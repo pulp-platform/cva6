8e3f25f6c699f921b86465e09cb0f5871fdc6270
Paul Scheffler <paulsc@iis.ee.ethz.ch>
2024-09-25 19:02:04 +0200

    [FIXME] id_stage: Work around simulator elab issue

--- core/id_stage.sv
+++ core/id_stage.sv
@@ -88,10 +88,10 @@ module id_stage #(
 );
   // ID/ISSUE register stage
   typedef struct packed {
-    logic              valid;
-    scoreboard_entry_t sbe;
-    logic [31:0]       orig_instr;
-    logic              is_ctrl_flow;
+    logic                                 valid;
+    logic [$bits(scoreboard_entry_t)-1:0] sbe;
+    logic [31:0]                          orig_instr;
+    logic                                 is_ctrl_flow;
   } issue_struct_t;
   issue_struct_t [CVA6Cfg.NrIssuePorts-1:0] issue_n, issue_q;
 
