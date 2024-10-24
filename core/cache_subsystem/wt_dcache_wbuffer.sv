5dfb8935db1bef2e7238b3025baf2d20b473784d
Paul Scheffler <paulsc@iis.ee.ethz.ch>
2024-10-24 18:42:31 +0200

    wt_dcache_buffer: Avoid out-of-range user signal access

--- core/cache_subsystem/wt_dcache_wbuffer.sv
+++ core/cache_subsystem/wt_dcache_wbuffer.sv
@@ -592,13 +592,9 @@ module wt_dcache_wbuffer
             wbuffer_d[wr_ptr].valid[k]     = 1'b1;
             wbuffer_d[wr_ptr].dirty[k]     = 1'b1;
             wbuffer_d[wr_ptr].data[k*8+:8] = req_port_i.data_wdata[k*8+:8];
-            if (CVA6Cfg.DATA_USER_EN) begin
-              wbuffer_d[wr_ptr].user[k*8+:8] = req_port_i.data_wuser[k*8+:8];
-            end else begin
-              wbuffer_d[wr_ptr].user[k*8+:8] = '0;
-            end
           end
         end
+        wbuffer_d[wr_ptr].user = CVA6Cfg.DATA_USER_EN ? req_port_i.data_wuser : '0;
       end
     end
   end
