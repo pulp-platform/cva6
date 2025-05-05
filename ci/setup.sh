3619124c2d86e03dca445920397afff7a5dd1e91
Riccardo Tedeschi <58978462+ricted98@users.noreply.github.com>
2025-05-05 16:40:26 +0200

    Remove edatools repository from prerequisites (#2953)

--- ci/setup.sh
+++ ci/setup.sh
@@ -10,8 +10,6 @@ export LD_LIBRARY_PATH=$RISCV/lib
 export C_INCLUDE_PATH=$RISCV/include
 export CPLUS_INCLUDE_PATH=$RISCV/include
 
-echo 'deb http://download.opensuse.org/repositories/home:/phiwag:/edatools/xUbuntu_20.04/ /' | sudo tee /etc/apt/sources.list.d/home:phiwag:edatools.list
-curl -fsSL https://download.opensuse.org/repositories/home:phiwag:edatools/Debian_Unstable/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_phiwag_edatools.gpg > /dev/null
 sudo apt update
 sudo apt install device-tree-compiler
 
