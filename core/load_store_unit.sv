1c6ffdca02528536ba03c467ca242afaa3fb621d
Enrico Zelioli <ezelioli@iis.ee.ethz.ch>
2025-05-09 12:44:19 +0200

    feat: add TLB locking
    Co-authored-by: Christopher Reinwardt <creinwar@iis.ee.ethz.ch>

--- core/load_store_unit.sv
+++ core/load_store_unit.sv
@@ -29,7 +29,9 @@ module load_store_unit
     parameter type acc_mmu_req_t = logic,
     parameter type acc_mmu_resp_t = logic,
     parameter type cbo_t = logic,
-    parameter type bp_resolve_t = logic
+    parameter type bp_resolve_t = logic,
+    parameter type pte_cva6_t = logic,
+    parameter type locked_tlb_entry_t = logic
 ) (
     // Subsystem Clock - SUBSYSTEM
     input logic clk_i,
@@ -122,6 +124,8 @@ module load_store_unit
     input  logic                                      vmxr_i,
     // TLB partitioning: currently allowed colors - CSR_REGFILE
     input logic            [CVA6Cfg.NumTlbColors-1:0] cur_clrs_i,
+    // TLB locking: locked TLB entries - CSR_REGFILE
+    input locked_tlb_entry_t [msb(CVA6Cfg.LockableTlbWays):0] locked_tlb_entries_i,
     // TO_BE_COMPLETED - TO_BE_COMPLETED
     input  logic             [      CVA6Cfg.PPNW-1:0] satp_ppn_i,
     // TO_BE_COMPLETED - TO_BE_COMPLETED
@@ -270,6 +274,8 @@ module load_store_unit
         .icache_drsp_t (icache_drsp_t),
         .dcache_req_i_t(dcache_req_i_t),
         .dcache_req_o_t(dcache_req_o_t),
+        .pte_cva6_t    (pte_cva6_t),
+        .locked_tlb_entry_t(locked_tlb_entry_t),
         .HYP_EXT       (HYP_EXT)
     ) i_cva6_mmu (
         .clk_i(clk_i),
@@ -308,6 +314,7 @@ module load_store_unit
         .hlvx_inst_i    (mmu_hlvx_inst),
         .hs_ld_st_inst_i(mmu_hs_ld_st_inst),
         .cur_clrs_i,
+        .locked_tlb_entries_i,
         .satp_ppn_i,
         .vsatp_ppn_i,
         .hgatp_ppn_i,
