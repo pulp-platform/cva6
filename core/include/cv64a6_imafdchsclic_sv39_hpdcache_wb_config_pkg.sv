69fe8ce3d078a8afb75bf2b0e87859f0b854077f
Thomas Benz <tbenz@iis.ee.ethz.ch>
2025-06-12 17:57:49 +0200

    Fix HPDcache config required in Cheshire

--- core/include/cv64a6_imafdchsclic_sv39_hpdcache_wb_config_pkg.sv
+++ core/include/cv64a6_imafdchsclic_sv39_hpdcache_wb_config_pkg.sv
@@ -76,7 +76,7 @@ package cva6_config_pkg;
 
   localparam CVA6ConfigPerfCounterEn = 1;
 
-  localparam config_pkg::cache_type_t CVA6ConfigDcacheType = config_pkg::HPDCACHE_WT;
+  localparam config_pkg::cache_type_t CVA6ConfigDcacheType = config_pkg::HPDCACHE_WB;
 
   localparam CVA6ConfigMmuPresent = 1;
 
