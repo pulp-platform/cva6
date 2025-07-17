a089c4b3c79cdc6a844479e7262ad3356c373dbe
Enrico Zelioli <ezelioli@iis.ee.ethz.ch>
2025-07-17 17:42:51 +0200

    feat: add hybrid cache / spm support to icache
    Co-authored-by: Christopher Reinwardt <creinwar@iis.ee.ethz.ch>

--- core/cva6.sv
+++ core/cva6.sv
@@ -640,6 +640,7 @@ module cva6
   logic [7:0] sintthresh_csr;
   logic [7:0] vsintthresh_csr;
   logic dcache_en_csr_nbdcache;
+  logic [CVA6Cfg.ICACHE_SET_ASSOC-1:0] icache_spm_ways_csr_cache;
   logic csr_write_fflags_commit_cs;
   logic icache_en_csr;
   logic acc_cons_en_csr;
@@ -1331,6 +1332,7 @@ module cva6
       .dcache_en_o             (dcache_en_csr_nbdcache),
       .cur_clrs_o              (cur_clrs_csr_ex),
       .locked_tlb_entries_o    (locked_tlb_entries_csr_ex),
+      .icache_spm_ways_o       (icache_spm_ways_csr_cache),
       .acc_cons_en_o           (acc_cons_en_csr),
       .fence_t_pad_o           (fence_t_pad_csr_ctrl),
       .fence_t_src_sel_o       (fence_t_src_sel_csr_ctrl),
@@ -1657,6 +1659,7 @@ module cva6
         .icache_en_i       (icache_en_csr),
         .icache_flush_i    (icache_flush_ctrl_cache),
         .icache_miss_o     (icache_miss_cache_perf),
+        .icache_spm_ways_i (icache_spm_ways_csr_cache),
         .icache_areq_i     (icache_areq_ex_cache),
         .icache_areq_o     (icache_areq_cache_ex),
         .icache_dreq_i     (icache_dreq_if_cache),
--- core/include/riscv_pkg.sv
+++ core/include/riscv_pkg.sv
@@ -685,6 +685,7 @@ package riscv;
     CSR_TLB_LOCK_VPN_8   = 12'h5D9,
     CSR_TLB_LOCK_ID_8    = 12'h5DA,
     // Cache Control (platform specifc)
+    CSR_ICACHE_SPM_WAYS  = 12'h5E1,
     CSR_DCACHE           = 12'h7C1,
     CSR_ICACHE           = 12'h7C0,
     // Accelerator memory consistency (platform specific)
