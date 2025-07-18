340876169adb5721bd0d75b58ab6dfe83242c760
Enrico Zelioli <ezelioli@iis.ee.ethz.ch>
2025-07-18 11:49:18 +0200

    feat: TLB coloring parametrization
    Co-authored-by: Christopher Reinwardt <creinwar@iis.ee.ethz.ch>

--- core/include/cv64a60ax_config_pkg.sv
+++ core/include/cv64a60ax_config_pkg.sv
@@ -138,6 +138,8 @@ localparam config_pkg::cva6_user_cfg_t cva6_cfg = '{
    FetchUserEn: unsigned'(0),
    InstrTlbEntries: int'(16),
    DataTlbEntries: int'(16),
+   TlbColoring: bit'(0),
+   NumTlbColors: int'(1),
    UseSharedTlb: bit'(0),
    SvnapotEn: bit'(0),
    SharedTlbDepth: int'(64),
--- core/include/deprecated_packages/cv32a6_embedded_config_pkg_deprecated.sv
+++ core/include/deprecated_packages/cv32a6_embedded_config_pkg_deprecated.sv
@@ -169,6 +169,8 @@ package cva6_config_pkg;
       FetchUserEn: unsigned'(CVA6ConfigFetchUserEn),
       InstrTlbEntries: int'(2),
       DataTlbEntries: int'(2),
+      TlbColoring: bit'(0),
+      NumTlbColors: int'(1),
       UseSharedTlb: bit'(1),
       SvnapotEn: bit'(0),
       SharedTlbDepth: int'(64),
