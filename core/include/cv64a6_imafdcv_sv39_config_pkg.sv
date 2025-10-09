77935019981aa3d628fabc850fec83fb0e7ca95c
Enrico Zelioli <ezelioli@iis.ee.ethz.ch>
2025-10-09 16:48:20 +0200

    feat: add vCLIC support (#55)

--- core/include/cv64a6_imafdcv_sv39_config_pkg.sv
+++ core/include/cv64a6_imafdcv_sv39_config_pkg.sv
@@ -31,6 +31,7 @@ package cva6_config_pkg;
   localparam CVA6ConfigVExtEn = 1;
   localparam CVA6ConfigRVZiCond = 0;
   localparam CVA6ConfigSclicExtEn = 0;
+  localparam CVA6ConfigXhclicExtEn = 0;
 
   localparam CVA6ConfigAxiIdWidth = 4;
   localparam CVA6ConfigAxiAddrWidth = 64;
@@ -108,6 +109,7 @@ package cva6_config_pkg;
       RVZCMP: bit'(CVA6ConfigZcmpExtEn),
       RVZCMT: bit'(0),
       RVSCLIC: bit'(CVA6ConfigSclicExtEn),
+      RVXHCLIC: bit'(CVA6ConfigXhclicExtEn),
       XFVec: bit'(CVA6ConfigFVecEn),
       CvxifEn: bit'(CVA6ConfigCvxifEn),
       CoproType: config_pkg::COPRO_NONE,
