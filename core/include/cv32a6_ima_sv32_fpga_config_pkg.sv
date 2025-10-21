264b760cb21e3851f4033e334e7fb02a8e7305e8
Enrico Zelioli <ezelioli@iis.ee.ethz.ch>
2025-10-21 11:44:33 +0200

    Hybrid cache/spm: parametrization
    Co-authored-by: Christopher Reinwardt <creinwar@iis.ee.ethz.ch>

--- core/include/cv32a6_ima_sv32_fpga_config_pkg.sv
+++ core/include/cv32a6_ima_sv32_fpga_config_pkg.sv
@@ -46,6 +46,9 @@ package cva6_config_pkg;
   localparam CVA6ConfigDcacheSetAssoc = 2;
   localparam CVA6ConfigDcacheLineWidth = 128;
 
+  localparam CVA6ConfigICacheSpmAddrBase = 56'h01A0_0000;
+  localparam CVA6ConfigDCacheSpmAddrBase = 56'h0180_0000;
+
   localparam CVA6ConfigDcacheIdWidth = 1;
   localparam CVA6ConfigMemTidWidth = 2;
 
@@ -137,9 +140,9 @@ package cva6_config_pkg;
       NrNonIdempotentRules: unsigned'(2),
       NonIdempotentAddrBase: 1024'({64'b0, 64'b0}),
       NonIdempotentLength: 1024'({64'b0, 64'b0}),
-      NrExecuteRegionRules: unsigned'(3),
-      ExecuteRegionAddrBase: 1024'({64'h8000_0000, 64'h1_0000, 64'h0}),
-      ExecuteRegionLength: 1024'({64'h40000000, 64'h10000, 64'h1000}),
+      NrExecuteRegionRules: unsigned'(4),
+      ExecuteRegionAddrBase: 1024'({64'h8000_0000, 64'(CVA6ConfigICacheSpmAddrBase), 64'h1_0000, 64'h0}),
+      ExecuteRegionLength: 1024'({64'h40000000, 64'(CVA6ConfigIcacheByteSize), 64'h10000, 64'h1000}),
       NrCachedRegionRules: unsigned'(1),
       CachedRegionAddrBase: 1024'({64'h8000_0000}),
       CachedRegionLength: 1024'({64'h40000000}),
@@ -154,6 +157,10 @@ package cva6_config_pkg;
       IcacheByteSize: unsigned'(CVA6ConfigIcacheByteSize),
       IcacheSetAssoc: unsigned'(CVA6ConfigIcacheSetAssoc),
       IcacheLineWidth: unsigned'(CVA6ConfigIcacheLineWidth),
+      ICacheSpmAddrBase: 56'(CVA6ConfigICacheSpmAddrBase),
+      ICacheSpmLength: 56'(CVA6ConfigIcacheByteSize),
+      DCacheSpmAddrBase: 56'(CVA6ConfigDCacheSpmAddrBase),
+      DCacheSpmLength: 56'(CVA6ConfigDcacheByteSize),
       DCacheType: CVA6ConfigDcacheType,
       DcacheByteSize: unsigned'(CVA6ConfigDcacheByteSize),
       DcacheSetAssoc: unsigned'(CVA6ConfigDcacheSetAssoc),
--- core/include/cv64a6_splus_config_pkg.sv
+++ core/include/cv64a6_splus_config_pkg.sv
@@ -45,6 +45,9 @@ package cva6_config_pkg;
   localparam CVA6ConfigDataUserEn = 0;
   localparam CVA6ConfigDataUserWidth = CVA6ConfigXlen;
 
+  localparam CVA6ConfigICacheSpmAddrBase = 56'h01A0_0000;
+  localparam CVA6ConfigDCacheSpmAddrBase = 56'h0180_0000;
+
   localparam CVA6ConfigIcacheByteSize = 16384;
   localparam CVA6ConfigIcacheSetAssoc = 4;
   localparam CVA6ConfigIcacheLineWidth = 128;
@@ -149,9 +152,9 @@ package cva6_config_pkg;
       NrNonIdempotentRules: unsigned'(2),
       NonIdempotentAddrBase: 1024'({64'b0, 64'b0}),
       NonIdempotentLength: 1024'({64'b0, 64'b0}),
-      NrExecuteRegionRules: unsigned'(3),
-      ExecuteRegionAddrBase: 1024'({64'h8000_0000, 64'h1_0000, 64'h0}),
-      ExecuteRegionLength: 1024'({64'h40000000, 64'h10000, 64'h1000}),
+      NrExecuteRegionRules: unsigned'(4),
+      ExecuteRegionAddrBase: 1024'({64'h8000_0000, 64'(CVA6ConfigICacheSpmAddrBase), 64'h1_0000, 64'h0}),
+      ExecuteRegionLength: 1024'({64'h40000000, 64'(CVA6ConfigIcacheByteSize), 64'h10000, 64'h1000}),
       NrCachedRegionRules: unsigned'(1),
       CachedRegionAddrBase: 1024'({64'h8000_0000}),
       CachedRegionLength: 1024'({64'h40000000}),
@@ -166,6 +169,10 @@ package cva6_config_pkg;
       IcacheByteSize: unsigned'(CVA6ConfigIcacheByteSize),
       IcacheSetAssoc: unsigned'(CVA6ConfigIcacheSetAssoc),
       IcacheLineWidth: unsigned'(CVA6ConfigIcacheLineWidth),
+      ICacheSpmAddrBase: 56'(CVA6ConfigICacheSpmAddrBase),
+      ICacheSpmLength: 56'(CVA6ConfigIcacheByteSize),
+      DCacheSpmAddrBase: 56'(CVA6ConfigDCacheSpmAddrBase),
+      DCacheSpmLength: 56'(CVA6ConfigDcacheByteSize),
       DCacheType: CVA6ConfigDcacheType,
       DcacheByteSize: unsigned'(CVA6ConfigDcacheByteSize),
       DcacheSetAssoc: unsigned'(CVA6ConfigDcacheSetAssoc),
