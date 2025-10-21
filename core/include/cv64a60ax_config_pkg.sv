09252e22d060e0b2b83e2f6dee8ae6dd019cb3ca
Enrico Zelioli <ezelioli@iis.ee.ethz.ch>
2025-10-21 11:36:23 +0200

    feat: TLB locking parametrization
    Co-authored-by: Christopher Reinwardt <creinwar@iis.ee.ethz.ch>

--- core/include/cv64a60ax_config_pkg.sv
+++ core/include/cv64a60ax_config_pkg.sv
@@ -140,6 +140,7 @@ localparam config_pkg::cva6_user_cfg_t cva6_cfg = '{
    DataTlbEntries: int'(16),
    TlbColoring: bit'(0),
    NumTlbColors: int'(1),
+   LockableTlbWays: int'(0),
    UseSharedTlb: bit'(0),
    SvnapotEn: bit'(0),
    SharedTlbDepth: int'(64),
