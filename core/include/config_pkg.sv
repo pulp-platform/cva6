0fb269c6a1776ef6f0ffd25a3246aff17d1e8111
Enrico Zelioli <ezelioli@iis.ee.ethz.ch>
2026-02-24 10:47:00 +0100

    Revert "feat: bump FPU to pulp-v0.2.3 (#25)"
    This reverts commit a27107b4a99001706d50a19196fbb3cac3eddc29.

--- core/include/config_pkg.sv
+++ core/include/config_pkg.sv
@@ -100,8 +100,6 @@ package config_pkg;
     bit                          XF16ALT;
     // Non standard 8bits Floating Point extension
     bit                          XF8;
-    // TO_BE_COMPLETED
-    bit                          XF8ALT;
     // Non standard Vector Floating Point extension
     bit                          XFVec;
     // Perf counters
@@ -304,7 +302,6 @@ package config_pkg;
     bit          XF16;
     bit          XF16ALT;
     bit          XF8;
-    bit          XF8ALT;
     bit          RVA;
     bit          RVB;
     bit          ZKN;
@@ -332,7 +329,6 @@ package config_pkg;
     bit          XF16Vec;
     bit          XF16ALTVec;
     bit          XF8Vec;
-    bit          XF8ALTVec;
     int unsigned NrRgprPorts;
     int unsigned NrWbPorts;
     bit          EnableAccelerator;
